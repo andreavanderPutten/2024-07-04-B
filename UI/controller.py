0da0afb5284de31327a15e630fcc391cfaac0f48
chiara vander <s264255@studenti.polito.it>
2024-09-08 15:22:18 +0200

    FINITO

--- UI/controller.py
+++ UI/controller.py
@@ -11,8 +11,52 @@ class Controller:
         self._model = model
 
     def handle_graph(self, e):
-        pass
+        if self._view.ddyear.value is None:
+            self._view.create_alert("Selezionare un anno!")
+            return
+        anno = int(self._view.ddyear.value)
+        if self._view.ddstate.value is None or self._view.ddstate.value == "":
+            self._view.create_alert("Selezionare uno stato!")
+            return
+        state = self._view.ddstate.value
+        self._view.txt_result1.controls.clear()
+        self._model.create_graph(anno, state)
+        self._view.txt_result1.controls.append(ft.Text(f"Numero di vertici: {self._model.get_num_of_nodes()}"))
+        self._view.txt_result1.controls.append(ft.Text(f"Numero di archi: {self._model.get_num_of_edges()}"))
+        self._view.txt_result1.controls.append(
+            ft.Text(f"Il grafo ha: {self._model.get_num_connesse()} componenti connesse"))
+        connessa = self._model.get_largest_connessa()
+        self._view.txt_result1.controls.append(ft.Text(f"La componente connessa più grande "
+                                                       f"è costituita da {len(connessa)} nodi:"))
+        for c in connessa:
+            self._view.txt_result1.controls.append(ft.Text(c))
+
+        self._view.btn_path.disabled = False
+        self._view.update_page()
 
     def handle_path(self, e):
-        pass
+        self._view.txt_result2.controls.clear()
+
+        path, punteggio = self._model.cammino_ottimo()
+        self._view.txt_result2.controls.append(ft.Text(f"Il punteggio del percorso ottimo è {punteggio}"))
+        self._view.txt_result2.controls.append(ft.Text(f"Il percorso ottimo è costituito da {len(path)} nodi:"))
+        for p in path:
+            self._view.txt_result2.controls.append(ft.Text(f"{p} - {p.duration}"))
+
+        self._view.update_page()
+
+    def fill_ddyear(self):
+        years = self._model.get_years()
+        self._view.ddyear.options.clear()
+        for y in years:
+            self._view.ddyear.options.append(ft.dropdown.Option(f"{y}"))
+
+    def fill_ddstate(self, e):
+        anno = int(self._view.ddyear.value)
+        self._view.ddstate.options.clear()
+        self._view.ddstate.value = None
+        shapes = self._model.get_states_year(anno)
+        for s in shapes:
+            self._view.ddstate.options.append(ft.dropdown.Option(key=s.id, text=s.name))
+        self._view.update_page()
 
--- database/DAO.py
+++ database/DAO.py
@@ -52,3 +52,74 @@ class DAO():
             cursor.close()
             cnx.close()
         return result
+
+    @staticmethod
+    def get_years():
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """SELECT DISTINCT YEAR(datetime) as anno 
+                    FROM sighting s 
+                    ORDER BY anno DESC"""
+            cursor.execute(query)
+
+            for row in cursor:
+                result.append(row["anno"])
+
+            cursor.close()
+            cnx.close()
+        return result
+
+    @staticmethod
+    def get_states_year(anno: int):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """SELECT DISTINCT st.*
+                       FROM sighting sig, state st 
+                       WHERE sig.state=st.id AND YEAR(sig.datetime)=%s
+                       ORDER BY Name ASC"""
+            cursor.execute(query, (anno,))
+
+            for row in cursor:
+                result.append(
+                    State(row["id"],
+                          row["Name"],
+                          row["Capital"],
+                          row["Lat"],
+                          row["Lng"],
+                          row["Area"],
+                          row["Population"],
+                          row["Neighbors"]))
+
+            cursor.close()
+            cnx.close()
+        return result
+
+
+    @staticmethod
+    def get_nodes(year: int, state: str):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """SELECT *
+                        FROM sighting s 
+                        WHERE Year(s.datetime)=%s AND s.state =%s
+                        ORDER BY s.datetime ASC"""
+            cursor.execute(query, (year, state,))
+
+            for row in cursor:
+                result.append(Sighting(**row))
+
+            cursor.close()
+            cnx.close()
+        return result
